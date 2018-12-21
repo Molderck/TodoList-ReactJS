413a1ce7cc30fdbec24b041fabeda4ad5a217eef
Molderck <sebastiansalazaro@hotmail.com>
2018-12-21 16:23:35 -0500

    Added states and handlers for creating

--- src/App.js
+++ src/App.js
@@ -6,13 +6,46 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import uuid from "uuid";
 
 class App extends Component {
+  state = {
+    items: [],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+
+  handleChange = event => {
+    //event.defaultPrevent();
+    this.setState({
+      item: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      item: this.state.item
+    };
+
+    const updatedItems = [...this.state.item, newItem];
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuid(),
+      editItem: false
+    });
+  };
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
             <h3 className="text-capitalize text-center">ToDo Input </h3>
-            <TodoInput />
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            />
             <TodoList />
           </div>
         </div>
